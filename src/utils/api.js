8b326569ebd79854a851e99c451208869a830e05
AyushGupta011 <ayuagrahari4@gmail.com>
2025-08-31 09:36:47 +0530

    changes in main.jsx

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,9 +25,9 @@
 // export default api;
 
 import axios from "axios";
-const API_BASE = process.env.REACT_APP_API_URL;
+// const API_BASE = process.env.REACT_APP_API_URL;
 const api = axios.create({
-  baseURL: "http://localhost:5050"||API_BASE,
+  baseURL: "http://localhost:5050",
    withCredentials: true,
 });
 
