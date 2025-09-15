4fa7b7ba68a7627ab130a4e30d7ae4954a400bbe
AyushGupta011 <ayuagrahari4@gmail.com>
2025-09-15 20:42:38 +0530

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const API_BASE = process.env.REACT_APP_API_URL;
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://noteshub-backend-cd3h.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL || "https://noteshub-backend-cd3h.onrender.com/"||"http://localhost:5050",
    withCredentials: true,
 });
 
